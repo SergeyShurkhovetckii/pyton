84fa319c5f8bb9e2c48a3327863782a04451285d
Sergei <S19S93@yandex.ru>
2021-02-24 07:51:21 +0200

    main

---
+++ pyton/fourth_lesson.py
@@ -0,0 +1,158 @@
+import math
+
+
+'''
+
+a = 25
+b = math.sqrt(a)
+print(b)
+'''
+'''
+a = [1,4,6,6,2,6]
+
+for i in a:
+	print(i)
+	if i == 6:
+		break
+'''
+
+
+'''
+a = [1,4,6,6,2,6]
+
+for i in a:
+	print(i)
+	if i == 6:
+		continue
+'''
+
+
+# a = int(input('Веедите a'))
+# b = int(input('Веедите b'))
+# c = int(input('Веедите c'))
+
+# discriminant = b**2 - 4*a*c
+# print("Дискриминант равен {}".format(discriminant))
+
+
+# if discriminant < 0:
+# 	print("Корней нет")
+
+
+# if 	discriminant > 0:
+# 	x1  = (-b + math.sqrt(discriminant)) / ( 2 * a)
+# 	x2  = (-b - math.sqrt(discriminant)) / ( 2 * a)
+# 	print("Ввыводим{x}".format (x=x1))
+# 	print("Ввыводим{x2}".format (x2=x2))
+
+
+# if 	discriminant == 0:
+# 	x = -b / ( 2 * a)
+# 	print("Ввыводим x {} ".format(x))
+
+
+# p = int(input("Введите показатель степени"))
+# n = int(input("Введите предел"))
+# i = 1
+
+# while i ** p <= n :
+# 	print(i**p)
+# 	i = i + 1
+
+
+# a = 'Число'
+
+# for i in a :
+# 	print(i)
+# 	if i =='и':
+# 		print(i)
+
+
+# 0 1 1 2 3 5 8 13
+
+# fib1 = 1
+# fib2 = 1 
+
+# end = int(input("До какого числа вывводить"))
+
+# i = 0 
+ 
+# while i < end :
+# 	fibsome = fib1 + fib2
+# 	fib1 = fib2
+# 	fib2 = fibsome
+# 	i = i + 1 
+# 	print(fib2)
+	
+##########################
+#функия 
+# def hceruge(a,b):
+#     c = a + b
+#     print(c)
+
+# def lo():
+#     c = 5+2
+#     print(c)
+
+
+# p = 3
+# n = 2
+
+# lo()
+###########################################
+# def plus(a,b):
+# 	c = a + b
+# 	print(c)
+
+
+# def munus(a,b):
+# 	c = a - b 	
+# 	print(c)
+
+
+# def umn (a,b):
+# 	c = a * b
+# 	print(c)
+
+
+# def	delum(a,b):
+# 	c = a / b 
+# 	print(c)
+	 	
+
+# a = int(input("Введите число 1"))
+# b = int(input("Введите число 2 "))
+
+# print("Что вы хотите сделать ")
+
+# action = input()
+
+# if action == "плюс" :
+# 	plus(a,b)
+
+# if action == "минус" :
+# 	munus(a,b)
+
+# if action == "умножение":
+# 	umn (a,b)
+
+# if action =="деление":
+# 	delum(a,b)	
+
+####################################################
+
+
+# a = int(input("Введите число 1"))
+
+# sume = 0 
+# mult = 1 
+
+# while a > 0 :
+# 	digit = a % 10 
+# 	sume  = sume + digit
+# 	mult = mult * digit
+# 	a = a // 10 
+# 	print(sume)
+# 	print(mult)
+
+
