3c9160fcea47559f6ca84854bddc867bf56ba00a
Sergei <S19S93@yandex.ru>
2021-03-08 10:32:28 +0200

    main

--- pyton/ДЗ-6.py
+++ pyton/ДЗ-6.py
@@ -1,12 +1,52 @@
 ###################
 #1
-def action (i , c ):
-    if i <= end :
-        print (i)
-    else:
 
 
-c = int(input("Введите знач"))
+#########################
+#2
+# def action():
+#     for i in a :
+#         if i =='ф' or i == 'б' or i == 'ч':
+#             print(i)
 
 
-action()
+# a = 'фибоначчи'
+
+# action()
+
+################################
+#3
+
+# def action ():
+#     print("привет", first_name ,last_name ,"твой пол", sex )
+
+
+# first_name = input("Введите свое имя")
+# last_name = input("Введите свою фамилию")
+# sex = input ("Введите свой пол")
+
+
+# action()
+#################################
+#4
+
+# def numb():
+#     i = a + b +c
+#     i = i / 3 
+#     print(i)
+   
+# a = int(input("Введите число1 "))
+# b = int(input("Введите число2 "))
+# c= int(input("Введите число3 "))
+
+# numb ()
+
+###################################
+#5
+
+
+
+
+
+
+
