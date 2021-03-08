aae4f28603f51c2bafd6108d573a6cf73f2c97f4
Sergei <S19S93@yandex.ru>
2021-03-08 09:00:38 +0200

    main

--- pyton/ДЗ-5.py
+++ pyton/ДЗ-5.py
@@ -19,9 +19,28 @@
 
 #############################################
 #3
+# 0 1 1 2 3 5 8 13
 
 
 
+# def fibonach(end):
+    
+#     fib1 = 1
+#     fib2 = 1 
+
+#     i = 0 
+    
+#     while i < end :
+#         fibsome = fib1 + fib2
+#         fib1 = fib2
+#         fib2 = fibsome
+#         i = i + 1 
+#         print(fib2)
+
+# end = int(input("До какого числа вывводить"))
+
+
+# fibonach(end)
 
 
 
@@ -29,11 +48,30 @@
 
 ##############################################
 #4
-a = 'Калькулятор'
 
-for i in a :
-    if i =='а':
-        print(i)
+
+# a = 'Калькулятор'
+
+# for i in a :
+#     if i =='а':
+#         print(i)
+
+
+# def actinon(a):
+#     for i in a :
+#         # or i =='р':
+#         if i =='а':
+#             print(i)
+#         if i =='р':
+#             print(i)    
+#         else :
+#             print(i)  
+
+# a = 'Калькулятор'
+
+# actinon(a)
+
+
 
 ##############################################
 #5
@@ -52,15 +90,75 @@ for i in a :
 #############################################
 #6
 
-def action (a) :
-    if a % 3 ==0:
-        print(a%3)
-    if a % 7 ==0 :     
-         print(a%7)
-    else :
-        print("число не делиться на 3 и на 7 ")     
+# def action (a) :
+#     if a % 3 ==0:
+#         print(a%3)
+#     if a % 7 ==0 :     
+#          print(a%7)
+#     else :
+#         print("число не делиться на 3 и на 7 ")     
 
         
-a = int(input("Введите число"))
+# a = int(input("Введите число"))
 
-action(a)
+# action(a)
+
+
+#################################################
+# def P (a):
+#     pr = 4 * a
+#     print(pr)
+
+
+# a = int(input("Введите знач"))
+
+# P(a)
+
+
+# def Sh (storon):
+#     S = storon**2
+#     print(S)
+
+# c = int(input("Введите знач"))
+
+# Sh(c)
+
+###################################################
+
+# # def action(a1 , a2 , a3 , a4 ,a5 ,a6 ):
+# #     a1 = a1*3
+# #     a2 = a2 *3
+# #     a3 = a3 *3
+# #     a4 = a4 *3
+# #     a5 = a5 *3
+# #     a6 = a6 *3
+# #     print(a1,a2,a3,a4,a5,a6)
+
+
+# # a = 1
+
+# # b = 3
+
+# # c = 5
+
+# # d = 7
+
+# # s = 1981
+
+# # f = 773
+
+
+# action(f,b,d,c,s,a)
+
+
+a = [1,332,4343,566,11,44]
+
+summ , i = 0 , 0 
+
+while i < len (a):
+    summ = summ + a[i]
+    print(summ) 
+    i = i + 1
+    print(i)
+
+# print()
---
+++ pyton/ДЗ-6.py
@@ -0,0 +1,12 @@
+###################
+#1
+def action (i , c ):
+    if i <= end :
+        print (i)
+    else:
+
+
+c = int(input("Введите знач"))
+
+
+action()
