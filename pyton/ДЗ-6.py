1aa2cfe8acfcc324089a13598caf4ad312607321
Sergei <S19S93@yandex.ru>
2021-03-09 19:53:42 +0200

    main

--- pyton/ДЗ-6.py
+++ pyton/ДЗ-6.py
@@ -1,6 +1,14 @@
 ###################
-#1
+# #1
+# def action ():
+#     for i in range(1 ,end):
+#         print(i )
 
+
+# end = int(input("Введите диопазон"))
+
+
+# action ()
 
 #########################
 #2
@@ -43,6 +51,67 @@
 
 ###################################
 #5
+
+# def numb ():
+#     i = a + b + c
+#     i = i % 3 == 0 
+#     print(i%3) 
+
+
+
+# a = int(input("Введите число1 "))
+# b = int(input("Введите число2 "))
+# c= int(input("Введите число3 "))
+
+
+# numb()
+
+
+###################################
+#6
+# def numb():
+#     if a % 2 == 0 :
+#         print("Число четное ")
+    
+#     else:
+#         print("Число не четное")
+
+
+# a = int(input("Введите число"))
+
+
+# numb()
+
+##################################
+#7
+def action():
+    discriminant = b**2 - 4*a*c
+    print("Дискриминант равен {}".format(discriminant))
+    if discriminant < 0:
+	    print("Корней нет")
+
+
+if 	discriminant > 0:
+	x1  = (-b + math.sqrt(discriminant)) / ( 2 * a)
+	x2  = (-b - math.sqrt(discriminant)) / ( 2 * a)
+	    print("Ввыводим{x}".format (x=x1))
+	    print("Ввыводим{x2}".format (x2=x2))
+
+
+ if  discriminant == 0:
+	x = -b / ( 2 * a)
+	    print("Ввыводим x {} ".format(x))
+
+
+
+
+
+
+a = int(input('Веедите a'))
+b = int(input('Веедите b'))
+c = int(input('Веедите c'))
+
+action()
 
 
 
