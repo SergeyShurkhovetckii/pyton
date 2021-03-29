e259d9123876f06023a0f20d9138e3983bc06d30
Sergei <S19S93@yandex.ru>
2021-03-29 20:12:29 +0200

    main

--- pyton/Home Work/ДЗ-8.py
+++ pyton/Home Work/ДЗ-8.py
@@ -1,12 +1,31 @@
-a = 'AB' * 52
-print(a)
+################################
+#2
+# a = 'AB' * 52
+# print(a)
+#
+# while 'AA' in a or 'BB' in a or 'AB' in a :
+#     if 'AA' in a :
+#         a=a.replace('AA', 'B',1)
+#     if 'BB' in a :
+#         a=a.replace('BB', 'A',1)
+#     if 'AB' in a :
+#         a=a.replace('AB','BA',1)
+#
+# print(a)
 
-while 'AA' in a or 'BB' in a or 'AB' in a :
-    if 'AA' in a :
-        a=a.replace('AA', 'B',1)
-    if 'BB' in a :
-        a=a.replace('BB', 'A',1)
-    if 'AB' in a :
-        a=a.replace('AB','BA',1)
+##################################
+#3
+s = '1' * 170 , '3' * 100 , '2'*7
+print(s)
 
-print(a)
+while '11' in s :
+    if '112' in s :
+        s=s. replase ('112','4' ,1)
+    if '113' in s :
+        s=s.replase ('113','2' ,1)
+    if '42' in s :
+        s=s.replase ('42','3' ,1)
+    if '43' in s:
+        s=s.replase ('43','1',1)
+
+print(s)
